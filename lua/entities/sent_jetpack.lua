9484484fa5bec489b906e61f88f9ba5c2ac1b684
Jvs <jvstheluacoder@gmail.com>
2014-10-16 19:02:12 +0200

    Jetpack stuff

--- lua/entities/sent_jetpack.lua
+++ lua/entities/sent_jetpack.lua
@@ -4,6 +4,15 @@ DEFINE_BASECLASS( "base_predictedent" )
 
 ENT.Spawnable = true
 ENT.SlotName = "jetpack"
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+sound.Add( {
+	name = "jetpack.thruster_loop",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 0.1,
+	sound = "^thrusters/jet02.wav"
+})
 
 function ENT:SpawnFunction( ply, tr, ClassName )
 
@@ -29,10 +38,26 @@ end
 
 function ENT:SetupDataTables()
 	BaseClass.SetupDataTables( self )
+	self:NetworkVar( "Bool" , 0 , "Active" )
 end
 
-if CLIENT then
+if SERVER then
+	function ENT:OnAttach( ply )
+		self:FollowBone( ply , ply:LookupBone("ValveBiped.Bip01_Spine2") )
+		self:SetLocalPos( vector_origin )
+		self:SetLocalAngles( angle_zero )
+	end
+else
 	function ENT:Draw( flags )
+		if self:GetControllingPlayer() == LocalPlayer() and not LocalPlayer():ShouldDrawLocalPlayer() then
+			return
+		end
 		self:DrawModel()
+	end
+	
+	function ENT:DrawTranslucent( )
+		if self:GetActive() then
+			--TODO: effects
+		end
 	end
 end
